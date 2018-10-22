37026ec44ef91701c16e849d694c0aa8279df52f
larissolb <42378253+larissolb@users.noreply.github.com>
2018-10-22 16:28:14 +0300

    Выполнены задача 1 и 2

--- tasks5.js
+++ tasks5.js
@@ -17,3 +17,23 @@ for (let i = 0; i < N; i++) {
 }
   }
 buildField(prompt('задайте кол-во ячеек в поле'));
+
+//Задача 2 про текущее время.
+
+function clock() {
+     'use strict';
+let now = new Date();
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+
+currentTime = "Текущее время - "+ hours + ":" + minutes + ":" + seconds;
+if (document.layers) {
+ document.layers.current-time.document.write(currentTime);
+ document.layers.current-time.document.close();
+}
+else document.getElementById("current-time").innerHTML = currentTime;
+ setInterval("clock()", 1000);
+}
+ clock();
