f73edc082a912a56b548c264a0b4712ac9f71175
larissolb <42378253+larissolb@users.noreply.github.com>
2018-10-22 13:06:13 +0300

    Create tasks5.js

---
+++ tasks5.js
@@ -0,0 +1,21 @@
+//Задача 1
+
+function buildField(N) {
+let rowDiv = document.createElement('div');
+    rowDiv.id = 'row';
+    let script = document.getElementsByTagName('script')[0];
+    document.body.insertBefore(rowDiv, script);
+// let N = 2;
+for (let i = 0; i < N; i++) {
+    let colDiv = document.createElement('div');
+    colDiv.id = 'col';
+    rowDiv.appendChild(colDiv);
+    for (let i = 0; i < N; i++) {   
+      let cellDiv = document.createElement('div');
+      cellDiv.id = 'cell';
+      colDiv.appendChild(cellDiv);
+  }
+}
+  }
+
+buildField(prompt('задайте кол-во ячеек в поле'));
