3724fcdc389ec09bb61899af8d3621625239f9f4
larissolb <fasolya@gmail.com>
2018-10-23 17:27:58 +0300

    исправлена 2 задача, выполнена 3 задача

--- tasks5.js
+++ tasks5.js
@@ -1,6 +1,7 @@
 //Задача 1
 
 function buildField(N) {
+      'use strict';
 let rowDiv = document.createElement('div');
     rowDiv.id = 'row';
     let script = document.getElementsByTagName('script')[0];
@@ -16,7 +17,7 @@ for (let i = 0; i < N; i++) {
   }
 }
   }
-buildField(prompt('задайте кол-во ячеек в поле'));
+buildField(prompt('задайте кол-во ячеек'));
 
 //Задача 2 про текущее время.
 
@@ -33,7 +34,49 @@ if (document.layers) {
  document.layers.current-time.document.write(currentTime);
  document.layers.current-time.document.close();
 }
-else document.getElementById("current-time").innerHTML = currentTime;
+else document.getElementById("currentTime").innerHTML = currentTime;
  setInterval("clock()", 1000);
 }
  clock();
+ 
+ //Задача 3 про вывод массива данных в таблицу
+ let users = createUserArr(1, 'Milk', 'fdhjfg@fhr.ru', '79558835', 'book');//  данные для вывода в таблицу
+function createUserArr(id, name, email, phone, position) {
+   let arr = [];
+   for (let i = 0; i < 100; i++){
+       arr.push(
+           {
+               id: i,
+               name: name,
+               email: email,
+               phone: phone,
+               position: position
+           }
+       )
+   }
+ 
+   return arr;
+}
+ let table = '<table>';
+table = table + '<thead>';
+table += '<tr>';
+table += '<td class="head">' + '№' + '</td>';
+table += '<td class="head">' + 'Имя' + '</td>';
+table += '<td class="head">' + 'email' + '</td>';
+table += '<td class="head">' + 'Телефон' + '</td>';
+table += '<td class="head">' + 'Должность' + '</td>';
+table += '</thead>';
+for(let i=0; i < users.length; i++ ) {
+    table += '<tbody>';
+    table += '<tr>';
+    table += '<td>' + users[i].id + '</td>';
+    table += '<td>' + users[i].name + '</td>';
+    table += '<td>' + users[i].email + '</td>';
+    table += '<td>' + users[i].phone + '</td>';
+    table += '<td>' + users[i].position + '</td>';
+    table += '</tr>';
+    table += '</tbody>';
+}
+    window.onload = function() {
+        document.getElementById('table').innerHTML = table + '</table>';
+    };
