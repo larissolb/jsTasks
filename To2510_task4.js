08a962a426efc8ed050591a6286bb81b58261e23
larissolb <42378253+larissolb@users.noreply.github.com>
2018-10-29 17:27:08 +0300

    Исправлены некоторые ошибки
    не конечная версия пока

--- To2510_task4.js
+++ To2510_task4.js
@@ -1,12 +1,15 @@
 // задание про генерацию игрового поля
 (function (global) {
 "use strict";
+
+//функция генерации поля  
 function buildField(N) {
- 
+
 let field = document.createElement('div');
     field.id = 'field';
     let script = document.getElementsByTagName('script')[0];
     document.body.insertBefore(field, script);
+    console.log(field);
 for (let i = 0; i < N; i++) {
     let colDiv = document.createElement('div');
     colDiv.id = 'col';
@@ -14,41 +17,45 @@ for (let i = 0; i < N; i++) {
     for (let i = 0; i < N; i++) {   
       let cellDiv = document.createElement('div');
       cellDiv.id = 'cell';
-      colDiv.appendChild(cellDiv);
+      colDiv.appendChild(cellDiv);    
   }
 }
   }
-buildField(prompt('задайте кол-во ячеек'));
-
-let col = document.getElementById('col'); // 
-console.log(col);
-let cells = col.children; // 
-console.log(cells);
-
-function generatePrize(col, prizeCount) {
+// buildField(prompt('задайте кол-во ячеек'));
+  buildField(4); //сгенерировано поле 4х4
+  
+  //Рандомное распределение призов
+function generatePrize(cells, prizeCount) {
     for (let i = 0; i <  prizeCount; i++){
-        col.children[Math.round(getRandom(0, col.children.length))]
-                  .classList.add("classname");
-            
-
-    }
+      cells[Math.round(getRandom(0, cells.length))].innerHTML= 'prize'; 
+      }
 }
-
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
+generatePrize(cell, 3); // размещено 3 случайных приза
 
-generatePrize(col, 4);
-
+//Предоставление 3-х попыток пользователю
 let field = document.getElementById('field');
-field.addEventListener('click', findPrize);
-
-function findPrize (event) {
-    if(cells.classList.contains("classname")) {
-        console.log('win!');
+// let attempts = 3; //кол-во попыток
+// field.onclick = function() { //счетчик количества кликов
+// if(attempts > 0) {
+//     attempts--;
+//     document.querySelector(".counter").innerHTML = ('Количество оставшихся попыток: ' + attempts); //вывод информации о кол-ве оставшихся попыток
+// } else {
+//   return alert('Game over'); //сообщение для пользователя о том, что игра окончена
+// }
+// }
+ 
+ field.onclick = function (event) {
+   
+  if(event.target.innerHTML === ('prize')) {
+     event.target.style.background = "blue";
+     // alert("prize!");
     } else {
-        console.log('no');
-    }
-}
+      event.target.style.background = "red";
+   }
+      
+ }
 
 }(window));
