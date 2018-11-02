686e86e74c68178e4bb2abfda6f1a7cd43bdd9d5
larissolb <fasolya@gmail.com>
2018-11-02 18:59:14 +0300

    исправлена задача про игру

--- To2510_task4.js
+++ To2510_task4.js
@@ -1,8 +1,7 @@
 // задание про генерацию игрового поля
-(function (global) {
 "use strict";
-
 //функция генерации поля  
+let prize;
 function buildField(N) {
 
 let field = document.createElement('div');
@@ -16,43 +15,48 @@ for (let i = 0; i < N; i++) {
     field.appendChild(colDiv);
     for (let i = 0; i < N; i++) {   
       let cellDiv = document.createElement('div');
-      cellDiv.id = 'cell';
+      cellDiv.classList.add('cell');
       colDiv.appendChild(cellDiv);    
-  }
 }
-  }
-// buildField(prompt('задайте кол-во ячеек'));
-  buildField(4); //сгенерировано поле 4х4
-  
-  //Рандомное распределение призов
-function generatePrize(cells, prizeCount) {
+}
+//Рандомное распределение призов
+prize = document.querySelectorAll(".cell");
+function generatePrize(prize, prizeCount) {
     for (let i = 0; i <  prizeCount; i++){
-      cells[Math.round(getRandom(0, cells.length))].innerHTML= ' '; 
+      prize[Math.round(getRandom(0, prize.length))].classList.add("cell-prize");
       }
+  function getRandom(min, max) {
+    return Math.random() * (max - min) + min; //x * (9-0) + 0
 }
-function getRandom(min, max) {
-    return Math.random() * (max - min) + min;
+ return;
 }
-generatePrize(cell, 3); // размещено 3 случайных приза
+generatePrize(prize, 3); // размещено 3 случайных приза
+console.log(prize);
+}
+
+// buildField(prompt('задайте кол-во ячеек'));
+   buildField(4); //сгенерировано поле 4х4
 
 //Предоставление 3-х попыток пользователю
 let field = document.getElementById('field');
 let attempts = 3; //кол-во попыток
+let win = 0; //счетчик, сколько угадано
 field.onclick = function(event) {//счетчик количества кликов
 if(attempts > 0) {
     attempts--;
     document.querySelector(".counter").innerHTML = ('Количество оставшихся попыток: ' + attempts); //вывод информации о кол-ве оставшихся попыток
- if(event.target.innerHTML === (' ')) { //если поле, на которое наведено содержит то же, что ячейка приза, то
-     event.target.innerHTML = "prize!"; //написать слово приз 
-      event.target.style.background = "blue"; //закрасить синим цветом
-     // alert("prize!");
-    } else {
+ if(event.target.classList.contains("cell-prize")) { //если поле, на которое наведено содержит то же, что ячейка приза, то
+    event.target.innerHTML = "prize!"; //написать слово приз 
+    event.target.style.background = "blue"; //закрасить синим цветом
+    win++;
+    } else  {
       event.target.style.background = "red"; //если не совпадает - закрасить красным цветом
    }
+   if(win === 3) {
+        alert ('You win!!!');//сообщение для пользователя о том, что он выиграл
+    } else if(attempts === 0) {
+  return alert('Game over, try again! Press F5'); //сообщение для пользователя о том, что игра окончена 
+} 
+}
+};
 
-} else {
-  return alert('Game over'); //сообщение для пользователя о том, что игра окончена
-}
-}
- 
-}(window));
